ca38703142eb5f114d6e2e8c5b8d436633b75906
Qué Código <quecodigoweb@gmail.com>
2022-04-08 12:59:56 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const port = process.env.PORT || 8080;
-const token = "EAAYMBvv1llABAI86GWtk1ETLzbzKap4tAxiZBc7jbzs6fX5brncF3ZA6KgjcJ9f7plGF8XvTlUEqBMuPbBJAbUZCyeAAKBvoI3WoYOPVdzDO2vy07NeJjDSKKXgT7KqcZBMvHgK2ds9vMFkDewl5khZCuuLzZBugmTZAW3mvZAc0FQZDZD";
+const token = "EAAYMBvv1llABADaUh3P0jIBbikf9Un4jKtNOYpWgEXwG8mZADZCpfsydDEO9hkzayPTu2zPIqKvq3DJOZAbbsnt8JDtFnsGXsWcQxr3a9Rbn24n0OHeUZAZBjzYSG7bBM6cKvg2heCoU7i7PoApPzsRIqP6EK6HYuoQyGOIuC7xVuR2kWzRFvPZAQ9ZBjpgkUPzk7WoDB1WCAZDZD";
 
 exports.port = port;
 exports.token = token;
